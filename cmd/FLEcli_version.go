134489772b54383608fc8932c024792f28a5136e
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2020-07-11 14:37:31 +0200

    Rename Version file

---
+++ cmd/FLEcli_version.go
@@ -0,0 +1,5 @@
+package cmd
+
+//VersionString is the version that will be displayed with the -v switch
+const VersionString = "v0.0.0  2020-07-11"
+
--- cmd/version2.go
+++
@@ -1,4 +0,0 @@
-package cmd
-
-const VersionString = "v0.0.0  2020-07-11"
-
