5c855f85fd2099056ff6b49c4ebeb9b87de066b2
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2020-07-13 13:59:40 +0200

    Improve versionning automation

--- cmd/FLEcli_version.go
+++ cmd/FLEcli_version.go
@@ -1,4 +1,4 @@
 package cmd
 
 //VersionString is the version that will be displayed with the -v switch
-const VersionString = "v0.0.0  2020-07-11"
+const VersionString = "v0.0.0.0-1-gb264805 (2020-07-13)"
