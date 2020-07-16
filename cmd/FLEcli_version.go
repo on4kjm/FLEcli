b4c740180a8b9939c2e4e0cd5290a6ae32567f98
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2020-07-16 20:27:51 +0200

    First preview release

--- cmd/FLEcli_version.go
+++ cmd/FLEcli_version.go
@@ -1,4 +1,4 @@
 package cmd
  
 //VersionString is the version that will be displayed with the -v switch
-const VersionString = "v0.0.0.0-15-g8c35ac4 (2020-07-16)"
+const VersionString = "v0.0.0 (2020-07-16)"
