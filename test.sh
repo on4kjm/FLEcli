dabf976add50ddd8646784cbccc6f758ae511a02
Jean-Marc Meessen <jean-marc@meessen-web.org>
2020-07-16 17:24:40 +0200

    Improve end-to-end testing

--- test.sh
+++ test.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-go build
-go test ./...
+./build.sh
+
+mkdir -p test/output/temp
 
 echo "--------------------------"
 ./FLEcli -i test/data/fle-1.txt load
 echo "--------------------------"
 ./FLEcli -i test/data/ON4KJM@ONFF-025920200524.txt load
 echo "--------------------------"
-./FLEcli -i test/data/sample_wwff_sota.txt load
+./FLEcli -i test/FLE-sample/sota_wwff.txt csv -o=test/output/temp/sota_wwff.csv --interpolate --overwrite
+diff test/output/temp/sota_wwff.csv test/FLE-sample/sota_wwff.csv --strip-trailing-cr && echo "no difference" || echo "differences!"
