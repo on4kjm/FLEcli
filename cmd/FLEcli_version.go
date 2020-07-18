f344690e2638aeb9f889315ab894e1a26fd6f134
Jean-Marc MEESSEN <jmeessen@cloudbees.com>
2020-07-18 21:27:57 +0200

    Implement Gorelease and github actions

--- cmd/FLEcli_version.go
+++ cmd/FLEcli_version.go
@@ -1,4 +1,4 @@
 package cmd
  
 //VersionString is the version that will be displayed with the -v switch
-const VersionString = "v0.0.0 (2020-07-16)"
+const VersionString = "v0.0.0-1-gb4c7401 (2020-07-18)"
