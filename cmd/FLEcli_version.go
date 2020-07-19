1a22646258b19f7ad247e9c13c4d168f6eab8095
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2020-07-19 09:36:40 +0200

    testing

--- cmd/FLEcli_version.go
+++
@@ -1,4 +0,0 @@
-package cmd
- 
-//VersionString is the version that will be displayed with the -v switch
-const VersionString = "v0.0.0-1-gb4c7401 (2020-07-18)"
--- cmd/root.go
+++ cmd/root.go
@@ -35,6 +35,7 @@ import (
 var cfgFile string
 var inputFilename string
 var isInterpolateTime bool
+var VersionString string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
