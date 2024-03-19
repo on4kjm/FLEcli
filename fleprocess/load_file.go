5b406438688ea6ee427bddd3cb3716e2cb8caa95
Nic Heath <nicheath@gmail.com>
2024-03-19 15:23:24 -0500

    Add support for MY_CNTY adif tag. (#128)
    * Add support for exporting MY_CNTY using mycounty in the header e.g. 'mycounty Ham County'.
    
    * Add MyCounty to displayLog in the nice console summary.
    
    ---------
    
    Co-authored-by: Jean-Marc MEESSEN <jean-marc@meessen-web.org>

--- fleprocess/load_file.go
+++ fleprocess/load_file.go
@@ -64,6 +64,7 @@ func LoadFile(inputFilename string, isInterpolateTime bool) (filleFullLog []LogL
 	regexpHeaderMySota := regexp.MustCompile(`(?i)^mysota\s+`)
 	regexpHeaderMyPota := regexp.MustCompile(`(?i)^mypota\s+`)
 	regexpHeaderMyGrid := regexp.MustCompile(`(?i)^mygrid\s+`)
+	regexpHeaderMyCounty := regexp.MustCompile(`(?i)^mycounty\s+`)
 	regexpHeaderQslMsg := regexp.MustCompile(`(?i)^qslmsg\s+`)
 	regexpHeaderNickname := regexp.MustCompile(`(?i)^nickname\s+`)
 
@@ -73,6 +74,7 @@ func LoadFile(inputFilename string, isInterpolateTime bool) (filleFullLog []LogL
 	headerMySOTA := ""
 	headerMyPOTA := ""
 	headerMyGrid := ""
+	headerMyCounty := ""
 	headerQslMsg := ""
 	headerNickname := ""
 	//headerDate := ""
@@ -245,6 +247,16 @@ func LoadFile(inputFilename string, isInterpolateTime bool) (filleFullLog []LogL
 			continue
 		}
 
+		//My County
+		if regexpHeaderMyCounty.MatchString(eachline) {
+			myMyCountyList := regexpHeaderMyCounty.Split(eachline, -1)
+			if len(myMyCountyList[1]) > 0 {
+				headerMyCounty = myMyCountyList[1]
+			}
+			//If there is no data after the marker, we just skip the data.
+			continue
+		}
+
 		//QSL Message
 		if regexpHeaderQslMsg.MatchString(eachline) {
 			myQslMsgList := regexpHeaderQslMsg.Split(eachline, -1)
@@ -283,6 +295,7 @@ func LoadFile(inputFilename string, isInterpolateTime bool) (filleFullLog []LogL
 		previousLogLine.MyPOTA = headerMyPOTA
 		previousLogLine.MySOTA = headerMySOTA
 		previousLogLine.MyGrid = headerMyGrid
+		previousLogLine.MyCounty = headerMyCounty
 		previousLogLine.QSLmsg = headerQslMsg //previousLogLine.QslMsg is redundant
 		previousLogLine.Nickname = headerNickname
 
